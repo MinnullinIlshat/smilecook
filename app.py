d65e7e8e6543511de3704007b2d7f95115eb628b
ilshat <minnullinilshat@gmail.com>
2023-02-21 17:55:02 +0300

    add jwt auth

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask_restful import Api
 from flask_migrate import Migrate
 
 from config import Config
-from extensions import db 
-from resources.user import UserListResource
+from extensions import db, jwt
+from resources.user import UserListResource, UserResource
 from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+from resources.token import TokenResource
 
 
 def create_app():
@@ -20,11 +21,16 @@ def create_app():
 def register_extensions(app):
     db.init_app(app)
     migrate = Migrate(app, db)
+    jwt.init_app(app)
 
 def register_resources(app):
     api = Api(app)
 
     api.add_resource(UserListResource, '/users')
+    api.add_resource(UserResource, '/users/<string:username>')
+
+    api.add_resource(TokenResource, '/token')
+
     api.add_resource(RecipeListResource, '/recipes')
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
     api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
